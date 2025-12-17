f29e5f8d2936129f4474b27ae335718606103a6f
vishvas.panshiniya <vishvas.panshiniya@esparkbizmail.com>
2025-12-17 19:02:02 +0530

    (VP) : fix config timeout issue

--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -6,33 +6,6 @@ import { IMailOptions } from '@common/types';
 import { parseInt } from '@common/utils';
 
 export const mailConfig = (options?: IMailOptions): MailerOptions => {
-  console.log('aa', {
-    host: options?.host ?? process.env.SMTP_HOST ?? 'localhost',
-    port: parseInt(options?.port ?? process.env?.SMTP_PORT) ?? 587,
-    secure: options?.port === 465, // true for 465, false for 587/other
-    requireTLS: true, // Force STARTTLS for port 587
-    // pool: true, // Enable connection pooling
-    maxConnections: 5, // Limit simultaneous connections
-    maxMessages: 100, // Max emails per connection
-
-    // ✅ REQUIRED FOR RENDER
-    connectionTimeout: 60000,
-    greetingTimeout: 60000,
-    socketTimeout: 60000,
-
-    tls: {
-      rejectUnauthorized: false, // Help with self-signed certs or proxy issues
-    },
-
-    auth: {
-      user: options?.user ?? process.env.SMTP_USER ?? 'user',
-      pass: options?.password ?? process.env.SMTP_PASSWORD ?? 'password',
-    },
-
-    logger: true, // Log to console
-    debug: true, // Include SMTP traffic in logs
-  });
-
   const mailConfiguration: MailerOptions = {
     transport: {
       host: options?.host ?? process.env.SMTP_HOST ?? 'localhost',
@@ -44,9 +17,9 @@ export const mailConfig = (options?: IMailOptions): MailerOptions => {
       maxMessages: 100, // Max emails per connection
 
       // ✅ REQUIRED FOR RENDER
-      connectionTimeout: 60_000,
-      greetingTimeout: 60_000,
-      socketTimeout: 60_000,
+      connectionTimeout: 60000,
+      greetingTimeout: 60000,
+      socketTimeout: 60000,
 
       tls: {
         rejectUnauthorized: false, // Help with self-signed certs or proxy issues
@@ -56,9 +29,6 @@ export const mailConfig = (options?: IMailOptions): MailerOptions => {
         user: options?.user ?? process.env.SMTP_USER ?? 'user',
         pass: options?.password ?? process.env.SMTP_PASSWORD ?? 'password',
       },
-
-      logger: true, // Log to console
-      debug: true, // Include SMTP traffic in logs
     },
     defaults: {
       from: {
